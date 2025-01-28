a6efab10c0d63164eddada1a169a04f03e384f61
ravynix <23743648+ravynix@users.noreply.github.com>
2025-01-28 12:01:20 +0700

    feat : update index js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const config = require('./config/index');
+const { fetchUserSleepData, fetchAllSleepData, batchDeleteSleepData } = require('./services/sleepData');
+const { calculateAverageSleepHours } = require('./services/dataProcessing');
+const { clearHistoricalCache } = require('./services/cache');
+
+const sdk = {
+    fetchUserSleepData,
+    fetchAllSleepData,
+    calculateAverageSleepHours,
+    batchDeleteSleepData,
+    clearHistoricalCache,
+};
 
 async function initialize() {
     console.log(`SDK Initialized in ${config.environment} mode.`);
